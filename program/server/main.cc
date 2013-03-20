e8f511cf053b31e4e0043ab12a87bb13162ee195
Linus Olofsson <dt09lo7@val-10.student.lth.se>
2013-03-20 14:18:24 +0100

    implemented a bit in main, server can be started and connected to as per example

--- program/server/main.cc
+++ program/server/main.cc
@@ -2,17 +2,47 @@
 #include <iostream>
 #include "in_memory_database.h"
 #include "server.h"
+#include "connection.h"
+#include "connectionclosedexception.h"
+
+int readNumber(client_server::Connection* conn) throw(client_server::ConnectionClosedException);
 
 //using namespace client_server;
 using namespace database;
 int main(){	
 	in_memory_database db();
-	std::cout << "Hejsan svejsans"<< std::endl;
 	client_server::Server s(2011);
 	if(!s.isReady()){
 		std::cerr << "Server could not be initialized" << std::endl;
+	}else{
+		std::cout <<"Server initialized"<< std::endl;
 	}
-	
+	while(true){
+		client_server::Connection* conn = s.waitForActivity();
+		if(conn != 0){
+			try{
+				//do connection stuff
+				int nbr = readNumber(conn);
+				std::cout << nbr << std::endl;
+			}catch(client_server::ConnectionClosedException&){
+				s.deregisterConnection(conn);
+				delete conn;
+				std::cout << "Client closed connection" << std::endl;
+			}
+		}else{
+			s.registerConnection(new client_server::Connection);
+			std::cout <<"New client connects"<< std::endl;
+		}
+	}
 	/* code */
 	return 0;
 }
+
+int readNumber(client_server::Connection* conn) throw(client_server::ConnectionClosedException) {
+	unsigned char byte1 = conn->read();
+	unsigned char byte2 = conn->read();
+	unsigned char byte3 = conn->read();
+	unsigned char byte4 = conn->read();
+	return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+}
+
