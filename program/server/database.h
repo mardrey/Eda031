cf40a71b08be3ba618be38e05fb5af1dbe9c4111
Petter Gulin <dt09pg9@val-8.student.lth.se>
2013-03-20 14:17:40 +0100

    added news group list

--- program/server/database.h
+++ program/server/database.h
@@ -1,14 +1,19 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 #include <string>
-
+#include "news_group.h"
+#include <vector>
 namespace database{
 class database{
 	public:
 	database();
 	~database();
+	void add_news_group(std::string& name);
 	virtual void print_status(int i) = 0;
 	private:
+	unsigned int news_group_ids;
+	std::vector<std::string> ngnames;
+	std::vector<news_group> ngroups;
 };
 }
 
