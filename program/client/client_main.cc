541b1892a9861ba6ddf08a560216a625f927a8ca
Petter Gulin <dt09pg9@falk-9.student.lth.se>
2013-03-22 13:01:09 +0100

    clientstuffs

---
+++ program/client/client_main.cc
@@ -0,0 +1,9 @@
+
+#include<iostream>
+
+int main(int argc, char* argv[]){
+	std::cout<<"\t\t\t\**************************************\t\t\t\n"<<"\t\t\t *Welcome to News Group Reader 2000* \t\t\t*"<<"\n\t\t\t\**************************************\t\t\t"<<std::endl;
+
+
+return 0;
+}
