022164e4045e1b30342a9a15aef923d1afbd9fef
Linus Olofsson <dt09lo7@falk-10.student.lth.se>
2013-03-22 13:16:35 +0100

    implemented mote in handler

--- program/client/client_main.cc
+++ program/client/client_main.cc
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]){
 
-	std::cout<<"\t\t\t\**************************************\t\t\t\n"<<"\t\t\t* Welcome to News Group Reader 2000  * \t\t\t"<<"\n\t\t\t\**************************************\t\t\t"<<std::endl;
+	std::cout<<"\t\t\t*************************************\t\t\t\n"<<"\t\t\t* Welcome to News Group Reader 2000  * \t\t\t"<<"\n\t\t\t*************************************\t\t\t"<<std::endl;
 	std::cout<<"\n\n\t\t\t\tPlease select a command:\n\n1:\tList News Groups\n2:\tCreate News Group\n3:\tDelete News Group\n4:\tCreate Article\n5:\tDelete Article\n6:\tGet Article"<<std::endl;
 
 
