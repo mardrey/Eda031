41030bcb86cbf0ad9d7d9c55ce4136a4b69a8777
Linus Olofsson <dt09lo7@falk-10.student.lth.se>
2013-03-22 13:04:54 +0100

    produced ok makefile for client, it now compiles

--- program/client/client_connection_handler.h
+++ program/client/client_connection_handler.h
@@ -1,6 +1,20 @@
+#include <string>
+#include "connection.h"
+
 namespace client{
 	class client_connection_handler{
+	public:
+		client_connection_handler(client_server::Connection* connection) : conn(connection){};
+		~client_connection_handler();
 
-		
+		bool send_command_list_ng();
+		bool send_command_create_ng(std::string ng_name);
+		bool send_command_delete_ng(unsigned int ng_id);
+		bool send_command_list_art(unsigned int ng_id);
+		bool send_command_create_art(unsigned int ng_id, std::string art_title, std::string art_author, std::string art_text);
+		bool send_command_delete_art(unsigned int ng_id, unsigned int art_id);
+		bool send_command_get_art(unsigned int ng_id, unsigned int art_id);
+	private:
+		client_server::Connection* conn;
 	};
 }
--- program/client/client_main.cc
+++ program/client/client_main.cc
@@ -1,8 +1,8 @@
 
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char* argv[]){
-	std::cout<<"\t\t\t\**************************************\t\t\t\n"<<"\t\t\t *Welcome to News Group Reader 2000* \t\t\t*"<<"\n\t\t\t\**************************************\t\t\t"<<std::endl;
+	std::cout<<"\t\t\t**************************************\t\t\t\n"<<"\t\t\t *Welcome to News Group Reader 2000* \t\t\t*"<<"\n\t\t\t**************************************\t\t\t"<<std::endl;
 
 
 return 0;
