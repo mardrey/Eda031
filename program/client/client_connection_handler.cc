b35d671059b31b217717e4dcc835c632bc836d48
Linus Olofsson <dt09lo7@venus-9.student.lth.se>
2013-04-09 14:07:56 +0200

    added ans_end to list_art

--- program/client/client_connection_handler.cc
+++ program/client/client_connection_handler.cc
@@ -203,6 +203,11 @@ bool client_connection_handler::send_command_list_art(unsigned int ng_id){
 			return false; //Command failed
 		}
 	}
+	com = conn->read();
+	if(com != protocol::Protocol::ANS_END){
+		std::cerr<<"Expected ans_end, got: "<<com<<std::endl;
+		return false;
+	}
 	std::cerr<<"Connection error"<<std::endl;
 	return false;
 }
